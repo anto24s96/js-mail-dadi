624b1f194731c0eae3902bca6d09b640e0e0fcf2
anto24s96 <anto24s96@gmail.com>
2023-11-23 18:33:18 +0100

    fix

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,7 +3,7 @@ let player = Math.floor((Math.random() * 6) + 1);
 console.log(player);
 
 /* CREO LA VARIABILE COMPUTER E GENERO UN NUMERO RANDOMICO DA 1-6*/
-let computer = Math.floor((Math.random() *6) + 1);
+let computer = Math.floor((Math.random() * 6) + 1);
 console.log(computer);
 
 /* CREO CONFRONTO TRA LE DUE VARIABILI */
