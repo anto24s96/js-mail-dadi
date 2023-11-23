4487c9b639e4769eac11be976239a02282de29f5
anto24s96 <anto24s96@gmail.com>
2023-11-23 19:30:39 +0100

    [FIX] exercise completed

--- mail/js/script.js
+++ mail/js/script.js
@@ -4,14 +4,19 @@ const emails = ["jason50@gmail.com", "david90@gmail.com", "simo90@gmail.com", "a
 /* Permettere all'utente di inserire l'email, creando un prompt; */
 let email_utente = prompt("Inserisci la tua e-mail");
 
+let validation = false;
+
 /* Creare un ciclo "for" per le email, e stampare; */
 for(let i = 0; i < emails.length; i++){
 
     if(email_utente == emails[i]){
-        console.log("Ben tornato!");
-    }
-    else{
-        console.log("e-mail non esistente!");
+        validation = true;
     }
 }
 
+if(validation){
+    alert("Ben tornato!");
+}
+else{
+    alert("Spiacenti, email non esistente");
+}
